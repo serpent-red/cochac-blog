7e82b8fee1eb6a69a1e21364c5c2b7baad8d783d
serpent-red <59523588+serpent-red@users.noreply.github.com>
2024-05-14 01:58:05 -0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,7 +3,7 @@ import os
 
 st.title("Circle of Care Health Advocacy Blog Page")
 
-post_dir = os.path.listdir("posts")
+post_dir = os.listdir("posts")
 
 if len(post_dir) == 0:
   st.write("No posts have been posted yet. Please check back later.")
