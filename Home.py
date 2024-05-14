2397c8e055517575cc1b2c5fcc02cbfd303b461b
serpent-red <59523588+serpent-red@users.noreply.github.com>
2024-05-14 01:51:06 -0400

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Circle of Care Health Advocacy Blog Page")
+
+post_dir = os.path.listdir("posts")
+
+if len(post_dir) == 0:
+  st.write("No posts have been posted yet. Please check back later.")
+else:
+  for p in post_dir:
+    st.write(p)
